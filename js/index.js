4c5e5042108260d20258cbc83ff2eb2a302322d0
dreamonaut <dreamonautre@gmail.com>
2025-07-27 20:22:55 +0200

    added templates

--- js/index.js
+++ js/index.js
@@ -1,69 +1,11 @@
  class CustomButton extends HTMLElement {
   constructor() {
-    super(); // Обязательный вызов конструктора родительского класса
+    super();
 
-    // Прикрепляем Shadow DOM к элементу
-    this.attachShadow({ mode: 'open' }); // 'open' - Shadow DOM доступен из JS, 'closed' - нет
+    this.attachShadow({ mode: 'open' });
 
-    // Клонируем содержимое шаблона и добавляем в Shadow DOM
-	  console.log('custom', document.getElementById('custom-button-template'));
     const template = document.getElementById('custom-button-template').content.cloneNode(true);
     this.shadowRoot.appendChild(template);
-
-    // Можно добавить обработчик кликов
-    this.shadowRoot.querySelector('button').addEventListener('click', this._handleClick.bind(this));
-  }
-
-  // Метод, который будет вызываться при клике
-  _handleClick(event) {
-    // Если кнопка disabled, не обрабатываем клик
-    if (this.hasAttribute('disabled')) {
-      event.stopPropagation(); // Предотвращаем всплытие события
-      event.preventDefault(); // Предотвращаем дефолтное действие
-      return;
-    }
-    // Вы можете пробросить кастомное событие наружу
-    this.dispatchEvent(new CustomEvent('button-click', {
-      bubbles: true, // Событие будет всплывать
-      composed: true, // Событие будет проходить через Shadow DOM
-      detail: {
-        message: 'Button was clicked!',
-        variant: this.getAttribute('variant'),
-        size: this.getAttribute('size')
-      }
-    }));
-  }
-
-  // Опционально: отслеживание атрибутов (если нужна JS-логика при их изменении)
-  // Для стилей это не обязательно, т.к. CSS :host([attribute]) справляется сам.
-  static get observedAttributes() {
-    return ['disabled']; // Например, чтобы React на изменение disabled
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'disabled') {
-      const buttonElement = this.shadowRoot.querySelector('button');
-      if (newValue !== null) { // Атрибут установлен (disabled="true" или просто disabled)
-        buttonElement.setAttribute('disabled', '');
-      } else { // Атрибут удален
-        buttonElement.removeAttribute('disabled');
-      }
-    }
-  }
-
-  // Пример геттера/сеттера для атрибутов, если хотите программный доступ
-  get variant() {
-    return this.getAttribute('variant');
-  }
-  set variant(value) {
-    this.setAttribute('variant', value);
-  }
-
-  get size() {
-    return this.getAttribute('size');
-  }
-  set size(value) {
-    this.setAttribute('size', value);
   }
 }
 
